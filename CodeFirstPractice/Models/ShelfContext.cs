f2e3a9cfe6a31bceb7067df01a177c82568250e6
AlecTech <eremeevoleg@gmail.com>
2020-10-30 15:59:36 -0600

    inherit from the DbContext, declare table DbSet, implement on configuring;

--- CodeFirstPractice/Models/ShelfContext.cs
+++ CodeFirstPractice/Models/ShelfContext.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CodeFirstPractice.Models
 {
-    class ShelfContext
+    class ShelfContext : DbContext 
     {
+        public virtual DbSet<Shelf> Shelfs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
     }
 }
