047630489d73a6de09a8c8cf72ab739646e8148e
AlecTech <eremeevoleg@gmail.com>
2020-10-30 15:40:34 -0600

    Defined table name and columns for shelfs table;

--- CodeFirstPractice/Models/Shelf.cs
+++ CodeFirstPractice/Models/Shelf.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CodeFirstPractice.Models
 {
-    class Shelf
+    /*
+     *  A database table called “Shelf” with:
+            An int primary key called ID.
+            A varchar of length 50 called Name.
+     * 
+     */
+    [Table("shelfs")]
+    public partial class Shelf
     {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+         
     }
 }
